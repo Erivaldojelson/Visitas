e860e1b99feb98474de49349e88063cf508279fb
Monst <monst@local>
2026-04-04 10:36:17 -0300

    Bump version to 1.7.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.monst.transfiranow"
         minSdk = 29
         targetSdk = 35
-        versionCode = 6
-        versionName = "1.7.0"
+        versionCode = 7
+        versionName = "1.7.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
